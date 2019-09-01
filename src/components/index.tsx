085ecf811c1e550f83d1a5294bb90ad0e20b9aec
shogo <114.s.aki@gmail.com>
2019-09-01 15:38:25 +0900

    feat: add styling with SSR

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,9 +2,16 @@
  * node_modules
  */
 import React, { FC } from "react"
+import styled from "styled-components"
 
 const Top: FC = props => (
-  <div>hoge!</div>
+  <StyledText>hoge!</StyledText>
 )
+
+const StyledText = styled.div`
+  color: red;
+  font-size: 30px;
+  font-weight: 600; 
+`
 
 export default Top
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@
  */
 import React from "react"
 import Document, { DocumentContext } from "next/document"
+import { ServerStyleSheet } from "styled-components"
 
 /**
  * layouts
@@ -12,8 +13,16 @@ import DefaultLayout from "../layouts"
 export default class extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
+    const styleSheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) => (props: any) => styleSheet.collectStyles(<App {...props} />)
+      })
+
     return {
-      ...initialProps
+      ...initialProps,
+      styles: [...(initialProps.styles as any), ...styleSheet.getStyleElement()],
     }
   }
 
